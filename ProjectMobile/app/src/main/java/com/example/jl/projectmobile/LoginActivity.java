8b5d624cfcd8831bc553cbf3c6ce1aa7a6bd5384
Christian Solís <crsc_26@hotmail.com>
2016-03-11 08:28:39 -0600

    Agrego código para poder hacer Log In con Facebook. Todavía no está listo

--- ProjectMobile/app/src/main/java/com/example/jl/projectmobile/LoginActivity.java
+++ ProjectMobile/app/src/main/java/com/example/jl/projectmobile/LoginActivity.java
@@ -1,9 +1,10 @@
 package com.example.jl.projectmobile;
 
+import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class LoginActivity extends AppCompatActivity {
+public class LoginActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
--- ProjectMobile/app/src/main/java/com/example/jl/projectmobile/SplashActivity.java
+++ ProjectMobile/app/src/main/java/com/example/jl/projectmobile/SplashActivity.java
@@ -2,10 +2,10 @@ package com.example.jl.projectmobile;
 
 import android.app.Activity;
 import android.content.Intent;
-import android.os.Handler;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.Window;
+import com.facebook.FacebookSdk;
 
 public class SplashActivity extends Activity {
     /** Duration of wait **/
