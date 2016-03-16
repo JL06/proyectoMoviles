c95e9b575b4f34d81a4e853f44d781d4fbcce78f
Christian Solís <crsc_26@hotmail.com>
2016-03-16 13:04:23 -0600

    Log In y Activity Main_events hasta ahorita

--- ProjectMobile/app/src/main/java/com/example/jl/projectmobile/LoginActivity.java
+++ ProjectMobile/app/src/main/java/com/example/jl/projectmobile/LoginActivity.java
@@ -3,12 +3,68 @@ package com.example.jl.projectmobile;
 import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.facebook.appevents.AppEventsLogger;
 
 public class LoginActivity extends Activity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState, LayoutInflater inflater, ViewGroup container) {
+
+        View view = inflater.inflate(R.layout.activity_splash, container, false);
+
+        loginButton = (LoginButton) view.findViewById(R.id.login_button);
+        loginButton.setReadPermissions("user_friends");
+        // If using in a fragment
+        loginButton.setFragment(this);
+        // Other app specific specialization
+
+        // Callback registration
+        loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                // App code
+            }
+
+            @Override
+            public void onCancel() {
+                // App code
+            }
+
+            @Override
+            public void onError(FacebookException exception) {
+                // App code
+            }
+        });
+    }
+
+
+
+
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        // Logs 'install' and 'app activate' App Events.
+        AppEventsLogger.activateApp(this);
+    }
+
+    // Agregar código abajo ↓ en cada actividad para rastrear
+    // el tiempo que pasa el usuario en la app.
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        // Logs 'app deactivate' App Event.
+        AppEventsLogger.deactivateApp(this);
     }
 }
